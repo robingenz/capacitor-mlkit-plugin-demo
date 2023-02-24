dae81cdd1401b74f187856126470221ac710443a
Robin Genz <mail@robingenz.dev>
2023-02-24 21:22:15 +0100

    feat(translation): manage models

--- src/app/modules/mlkit-translation/mlkit-translation.module.ts
+++ src/app/modules/mlkit-translation/mlkit-translation.module.ts
@@ -1,20 +1,12 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-
-import { IonicModule } from '@ionic/angular';
+import { SharedModule } from '@app/shared';
 
 import { MlkitTranslationPageRoutingModule } from './mlkit-translation-routing.module';
 
 import { MlkitTranslationPage } from './mlkit-translation.page';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    MlkitTranslationPageRoutingModule,
-  ],
+  imports: [SharedModule, MlkitTranslationPageRoutingModule],
   declarations: [MlkitTranslationPage],
 })
 export class MlkitTranslationPageModule {}
--- src/app/modules/mlkit-translation/mlkit-translation.page.ts
+++ src/app/modules/mlkit-translation/mlkit-translation.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UntypedFormControl, UntypedFormGroup } from '@angular/forms';
 import { Language, Translation } from '@capacitor-mlkit/translation';
 
@@ -7,7 +7,7 @@ import { Language, Translation } from '@capacitor-mlkit/translation';
   templateUrl: './mlkit-translation.page.html',
   styleUrls: ['./mlkit-translation.page.scss'],
 })
-export class MlkitTranslationPage {
+export class MlkitTranslationPage implements OnInit {
   public readonly language = Language;
   public translateFormGroup = new UntypedFormGroup({
     text: new UntypedFormControl(''),
@@ -16,15 +16,48 @@ export class MlkitTranslationPage {
     translatedText: new UntypedFormControl(''),
   });
   public manageModelsFormGroup = new UntypedFormGroup({
-    language: new UntypedFormControl(Language.English),
+    languages: new UntypedFormControl([]),
   });
+  public disableSaveModelsButton = false;
 
   private readonly githubUrl = 'https://github.com/robingenz/capacitor-mlkit';
 
   constructor() {}
 
+  public ngOnInit(): void {
+    this.getDownloadedModels();
+  }
+
   public openOnGithub(): void {
     window.open(this.githubUrl, '_blank');
+  }
+
+  public async saveModels(): Promise<void> {
+    this.disableSaveModelsButton = true;
+    const languages: Language[] =
+      this.manageModelsFormGroup.get('languages')?.value;
+    if (!languages) {
+      return;
+    }
+    const promises = [];
+    for (const availableLanguage of Object.values(Language)) {
+      let promise;
+      if (languages.includes(availableLanguage)) {
+        promise = Translation.downloadModel({ language: availableLanguage });
+      } else {
+        promise = Translation.deleteDownloadedModel({
+          language: availableLanguage,
+        });
+      }
+      promises.push(promise);
+    }
+    await Promise.all(promises);
+    this.disableSaveModelsButton = false;
+  }
+
+  public async getDownloadedModels(): Promise<void> {
+    const { languages } = await Translation.getDownloadedModels();
+    this.manageModelsFormGroup.patchValue({ languages });
   }
 
   public async translate(): Promise<void> {
@@ -40,13 +73,5 @@ export class MlkitTranslationPage {
       targetLanguage,
     });
     this.translateFormGroup.patchValue({ translatedText: result.text });
-  }
-
-  public async deleteDownloadedModel(): Promise<void> {
-    const language = this.manageModelsFormGroup.get('language')?.value;
-    if (!language) {
-      return;
-    }
-    await Translation.deleteDownloadedModel({ language });
   }
 }
